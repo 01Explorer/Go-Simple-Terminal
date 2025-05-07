f4e025d58555678ddcf4c3f6cb96d1e1c53bda22
Gabriel <gabrielsantostca@gmail.com>
2025-05-06 23:46:06 -0300

    feat: made the tabs interactive

--- main.go
+++ main.go
@@ -1,147 +1,101 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
-	"os/exec"
 	"strings"
-	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/charmbracelet/x/term"
-	"github.com/creack/pty"
 )
 
-type model struct {
-  choices []string
-  cursor int
-  selected map[int]struct{}
+type MainModel struct {
+  Tabs []string
+  TabContent []string
+  activeTab int
 }
 
-func initialModel()  model {
-  return model{
-    choices: []string{"Buy carrots", "Buy celery", "Buy kohlraby"},
-    selected: make(map[int]struct{}),
-  }
+func (m MainModel) Init() tea.Cmd {
+ return nil 
 }
 
-func (m model) Init() tea.Cmd {
-  return nil 
-}
-
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd){
+func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd){
   switch msg := msg.(type) {
- case tea.KeyMsg:
-   switch msg.String() {
-   case "ctrl+c", "q":
-     return m, tea.Quit
-  case "up", "k":
-   if m.cursor > 0  {
-     m.cursor--
-   } 
- case "down", "j":
-   if m.cursor < len(m.choices)-1 {
-     m.cursor++
-    
-   }
- case "enter", " ":
-   _, ok := m.selected[m.cursor]
-   if ok {
-     delete(m.selected, m.cursor)
-   } else {
-     m.selected[m.cursor] = struct{}{}
-   }
-   }
- }
+  case tea.KeyMsg:
+    switch keypress := msg.String(); keypress {
+    case "q", "ctrl+c":
+      return m, tea.Quit
+    case "right", "l":
+      m.activeTab = min(m.activeTab+1, len(m.Tabs)-1)
+      return m, nil
+    case "left", "n":
+      m.activeTab = max(m.activeTab-1, 0)
+      return m, nil
+    }
+  }
+  return m, nil
+}
 
-return m, nil
- }
+func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
+  border := lipgloss.RoundedBorder()
+  border.BottomLeft = left
+  border.Bottom = middle
+  border.BottomRight = right
+  return border
+}
 
- func (m model) View()  string {
-   s := "What should we buy at the market??\n\n"
+var (
+  inactiveTabBorder = tabBorderWithBottom("┴", "─", "┴")
+	activeTabBorder   = tabBorderWithBottom("┘", " ", "└")
+	docStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
+	highlightColor    = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+	inactiveTabStyle  = lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(highlightColor).Padding(0, 1)
+	activeTabStyle    = inactiveTabStyle.Border(activeTabBorder, true)
+	windowStyle       = lipgloss.NewStyle().BorderForeground(highlightColor).Padding(2, 0).Align(lipgloss.Center).Border(lipgloss.NormalBorder()).UnsetBorderTop()
 
-   for i, choice := range m.choices {
-     cursor := " "
-     if m.cursor == i {
-       cursor = ">"
-     }
+)
 
-     checked := " " 
-     if _, ok := m.selected[i]; ok {
-       checked = "x"
-     }
-     s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-   }
-
-   s += "\nPress q to quit.\n"
-
-   return s
- }
-
-func main() {
-  docStyle := lipgloss.NewStyle().Padding(1, 2, 1, 2)
-  physicalWidth, _, _ := term.GetSize(os.Stdout.Fd())
+func (m MainModel) View() string {
   doc := strings.Builder{}
 
-  row := lipgloss.JoinHorizontal(
-    lipgloss.Top,
-    activeTab.Render("Lip Gloss"),
-    tab.Render("Blush"),
-    tab.Render("Eye Shadow"),
-    tab.Render("Mascara"),
-    tab.Render("Foundation"),
-  )
-  gap := tabGap.Render(strings.Repeat(" ", max(0, 80 - lipgloss.Width(row) - 2)))
-  row = lipgloss.JoinHorizontal(lipgloss.Bottom, row, gap)
-  doc.WriteString(row + "\n\n")
+  var renderedTabs []string
 
-  if physicalWidth > 0 {
-    docStyle = docStyle.MaxWidth(physicalWidth)
+  for i, t := range m.Tabs {
+    var style lipgloss.Style
+    isFirst, isLast, isActive := i == 0, i == len(m.Tabs) - 1, i == m.activeTab
+
+    style = inactiveTabStyle
+    if isActive {
+     style = activeTabStyle 
+    }
+
+    border, _, _, _, _ := style.GetBorder()
+    if (isFirst || isLast) && isActive {
+      border.BottomLeft = "|"
+    } else if isFirst && !isActive {
+    	border.BottomLeft = "├"
+    } else if isLast && !isActive {
+      border.BottomRight = "┤"
+    }
+    style = style.Border(border)
+    renderedTabs = append(renderedTabs, style.Render(t))
   }
 
-  fmt.Println(docStyle.Render(doc.String()))
+  row := lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs...)
+  doc.WriteString(row)
+  doc.WriteString("\n")
+  doc.WriteString(windowStyle.Width((lipgloss.Width(row)-windowStyle.GetHorizontalFrameSize())).Render(m.TabContent[m.activeTab]))
+  return docStyle.Render(doc.String())
+}
 
-  p := tea.NewProgram(New())
-  if _, err := p.Run(); err != nil {
-    fmt.Printf("Alas, there's been an error: %v", err)
+
+func main() {
+  tabs := []string{"Pomodoro", "Short Rest", "Long Rest"}
+  tabContent := []string{"Pomodoro configured Timer", "Shor Rest Timer", "Long Rest Timer"}
+  m := MainModel{Tabs: tabs, TabContent: tabContent}
+  if _, err := tea.NewProgram(m).Run(); err != nil {
+    fmt.Println("Error running program:", err)
     os.Exit(1)
   }
 }
   
-func oldMain() {
-  cmd := exec.Command("/bin/bash")
-  pty, err := pty.Start(cmd)
-  if err != nil {
-    panic(err)
-  }
-  defer pty.Close()
-
-  reader := bufio.NewReader(pty)
-  go func() {
-   for {
-     buf := make([]byte, 1024)
-     n, err := reader.Read(buf)
-     if err != nil {
-       if err == io.EOF {
-         return
-       }
-       panic(err)
-     }
-     os.Stdout.Write(buf[:n])
-   } 
-  }()
-
-  go func() {
-    scanner := bufio.NewScanner(os.Stdin) 
-    for scanner.Scan(){
-      input := scanner.Text()
-      pty.Write([]byte(input + "\n"))
-    }
-  }()
-
-  time.Sleep(10 * time.Second)
-}
