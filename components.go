ac1a626f0f42d39026ced27b9cebad02f011f873
Gabriel <gabrielsantostca@gmail.com>
2025-06-10 22:54:53 -0300

    feat: created specific file to create view components

---
+++ components.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+func CenterSquareWithText(appWidth, appHeight, outerWidth, outerHeight, innerWidth, innerHeight int, content string) string {
+  outerStyle := lipgloss.NewStyle().
+  Width(outerWidth).
+  Height(outerHeight).
+  Border(lipgloss.RoundedBorder()).
+  BorderForeground(lipgloss.Color("#E95420"))
+
+  innerRendered := lipgloss.Place(outerWidth, outerHeight, lipgloss.Center, lipgloss.Center, content)
+  return outerStyle.Render(innerRendered)
+}
+
+func CenterTabsAboveSquare(appWidth, appHeight, squareWidth, squareHeight int, tabs, square string) string {
+  squareLeft := (appWidth - squareWidth) / 2
+  //squareTop := (appHeight - squareHeight) / 2
+
+  //tabsTop := squareTop - 3
+
+  tabsStyle := lipgloss.NewStyle().
+  MarginLeft(squareLeft)
+
+  squareStyle := lipgloss.NewStyle().
+  MarginLeft(squareLeft)
+
+  combined := lipgloss.JoinVertical(
+    lipgloss.Left,
+    tabsStyle.Render(tabs),
+    squareStyle.Render(square),
+  )
+
+  return combined
+}
