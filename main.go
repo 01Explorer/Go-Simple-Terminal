2c3d650d2009784b84e7367c9d2a79b0ed54045e
Gabriel <gabrielsantostca@gmail.com>
2025-06-10 22:57:42 -0300

    refactor: changed main to use new structure

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -11,8 +10,10 @@ import (
 
 type MainModel struct {
   Tabs []string
-  TabContent []string
+  TabContent []Model
   activeTab int
+  width int
+  height int
 }
 
 func (m MainModel) Init() tea.Cmd {
@@ -27,13 +28,16 @@ func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd){
       return m, tea.Quit
     case "right", "l":
       m.activeTab = min(m.activeTab+1, len(m.Tabs)-1)
-      return m, nil
     case "left", "n":
       m.activeTab = max(m.activeTab-1, 0)
-      return m, nil
     }
+   case tea.WindowSizeMsg:
+     m.width = msg.Width
+     m.height = msg.Height
   }
-  return m, nil
+  model, cmd := m.TabContent[m.activeTab].Update(msg)
+  m.TabContent[m.activeTab] = model.(Model)
+  return m, cmd 
 }
 
 func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
@@ -45,53 +49,63 @@ func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
 }
 
 var (
-  inactiveTabBorder = tabBorderWithBottom("┴", "─", "┴")
-	activeTabBorder   = tabBorderWithBottom("┘", " ", "└")
 	docStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
-	highlightColor    = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
-	inactiveTabStyle  = lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(highlightColor).Padding(0, 1)
-	activeTabStyle    = inactiveTabStyle.Border(activeTabBorder, true)
+	highlightColor    = lipgloss.Color("#E95420")
+  inactiveTabStyle  = lipgloss.NewStyle()
+	activeTabStyle    = inactiveTabStyle.Background(highlightColor)
 	windowStyle       = lipgloss.NewStyle().BorderForeground(highlightColor).Padding(2, 0).Align(lipgloss.Center).Border(lipgloss.NormalBorder()).UnsetBorderTop()
 
 )
 
 func (m MainModel) View() string {
-  doc := strings.Builder{}
+  outerWidth := int(float64(m.width) * 0.8)
+  outerHeight := int(float64(m.height) * 0.8)
+  innerWidth := outerWidth - 4
+  innerHeight := 15
+
+  // doc := strings.Builder{}
 
   var renderedTabs []string
 
   for i, t := range m.Tabs {
     var style lipgloss.Style
-    isFirst, isLast, isActive := i == 0, i == len(m.Tabs) - 1, i == m.activeTab
+    isActive := i == m.activeTab
 
     style = inactiveTabStyle
     if isActive {
      style = activeTabStyle 
     }
 
-    border, _, _, _, _ := style.GetBorder()
-    if (isFirst || isLast) && isActive {
-      border.BottomLeft = "|"
-    } else if isFirst && !isActive {
-    	border.BottomLeft = "├"
-    } else if isLast && !isActive {
-      border.BottomRight = "┤"
-    }
-    style = style.Border(border)
+    style = style.Border(lipgloss.RoundedBorder()).
+    Width((innerWidth - 1)/3).
+    Height(2).
+    Align(lipgloss.Center, lipgloss.Center).
+    BorderForeground(highlightColor)
+    
     renderedTabs = append(renderedTabs, style.Render(t))
   }
 
-  row := lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs...)
-  doc.WriteString(row)
-  doc.WriteString("\n")
-  doc.WriteString(windowStyle.Width((lipgloss.Width(row)-windowStyle.GetHorizontalFrameSize())).Render(m.TabContent[m.activeTab]))
-  return docStyle.Render(doc.String())
+  row := lipgloss.JoinHorizontal(lipgloss.Left, renderedTabs...)
+  activeTab := m.TabContent[m.activeTab].View()
+  // doc.WriteString(row)
+  // doc.WriteString("\n")
+  // doc.WriteString(m.TabContent[m.activeTab].View())
+
+  mainSquare := CenterSquareWithText(m.width, m.height, outerWidth, outerHeight, innerWidth, innerHeight, activeTab)
+
+  squareWithTab := CenterTabsAboveSquare(m.width, m.height, outerWidth, outerHeight, row, mainSquare)
+
+  return squareWithTab
 }
 
 
 func main() {
-  tabs := []string{"Pomodoro", "Short Rest", "Long Rest"}
-  tabContent := []string{"Pomodoro configured Timer", "Shor Rest Timer", "Long Rest Timer"}
+  tabs := []string{"Pomodoro", "Short Break", "Long Break"}
+  tabContent := []Model {
+    New(Pomodoro()),
+    New(ShortBreak()),
+    New(LongBreak()),
+  }
   m := MainModel{Tabs: tabs, TabContent: tabContent}
   if _, err := tea.NewProgram(m).Run(); err != nil {
     fmt.Println("Error running program:", err)
